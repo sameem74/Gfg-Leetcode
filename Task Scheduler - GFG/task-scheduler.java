7d47235c43be4f5c8490ef94d55c1e53dc47bc73
Sameem Ahamad <118834172+sameem74@users.noreply.github.com>
2023-06-24 14:17:54 +0530

    Added solution - LeetHub

---
+++ Task Scheduler - GFG/task-scheduler.java
@@ -0,0 +1,119 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        FastReader sc = new FastReader(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            char a[] = new char[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.next().charAt(0);
+            }
+            Solution obj = new Solution();
+            int ans = obj.leastInterval(n, k, a);
+            System.out.println(ans);
+        }
+    }
+    static class FastReader {
+
+        byte[] buf = new byte[2048];
+        int index, total;
+        InputStream in;
+
+        FastReader(InputStream is) { in = is; }
+
+        int scan() throws IOException {
+            if (index >= total) {
+                index = 0;
+                total = in.read(buf);
+                if (total <= 0) {
+                    return -1;
+                }
+            }
+            return buf[index++];
+        }
+
+        String next() throws IOException {
+            int c;
+            for (c = scan(); c <= 32; c = scan())
+                ;
+            StringBuilder sb = new StringBuilder();
+            for (; c > 32; c = scan()) {
+                sb.append((char)c);
+            }
+            return sb.toString();
+        }
+
+        int nextInt() throws IOException {
+            int c, val = 0;
+            for (c = scan(); c <= 32; c = scan())
+                ;
+            boolean neg = c == '-';
+            if (c == '-' || c == '+') {
+                c = scan();
+            }
+            for (; c >= '0' && c <= '9'; c = scan()) {
+                val = (val << 3) + (val << 1) + (c & 15);
+            }
+            return neg ? -val : val;
+        }
+
+        long nextLong() throws IOException {
+            int c;
+            long val = 0;
+            for (c = scan(); c <= 32; c = scan())
+                ;
+            boolean neg = c == '-';
+            if (c == '-' || c == '+') {
+                c = scan();
+            }
+            for (; c >= '0' && c <= '9'; c = scan()) {
+                val = (val << 3) + (val << 1) + (c & 15);
+            }
+            return neg ? -val : val;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int leastInterval(int N, int K, char tasks[]) {
+        // code here
+        int[] counter = new int[26];
+        int maxF = 0; 
+        int maxFc = 0; 
+        for (char task : tasks) {
+            counter[task - 'A']++;
+        }
+        for (int x : counter)
+        {
+            if(maxF==x)
+            {
+                 maxFc++;
+            }
+            if(maxF<x)
+            {
+                maxF=x;
+                maxFc=1;
+            }
+        }
+        int gaps = maxF-1;
+        int gaps_len = K - (maxFc - 1);
+        int avail_slot = gaps * gaps_len;
+        int avail_task = N - maxF * maxFc;
+        int idles = Math.max(0, avail_slot - avail_task);
+        return N+idles;
+    }
+}
