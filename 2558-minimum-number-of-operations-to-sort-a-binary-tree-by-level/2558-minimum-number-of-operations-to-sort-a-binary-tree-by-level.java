d3cbbd7a2e7ae72b6d6103b9c6825699b0a24d92
Sameem Ahamad <118834172+sameem74@users.noreply.github.com>
2024-12-23 13:36:54 +0530

    Time: 70 ms (46.58%), Space: 66.8 MB (58.9%) - LeetHub

---
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.java
@@ -0,0 +1,30 @@
+class Solution {
+  public int minimumOperations(TreeNode root) {
+    int ans = 0;
+    Queue<TreeNode> q = new LinkedList<>(Arrays.asList(root));
+    while (!q.isEmpty()) {
+      List<Integer> vals = new ArrayList<>();
+      List<Integer> ids = new ArrayList<>();
+      for (int sz = q.size(); sz > 0; --sz) {
+        TreeNode node = q.poll();
+        vals.add(node.val);
+        if (node.left != null)
+          q.offer(node.left);
+        if (node.right != null)
+          q.offer(node.right);
+      }
+      for (int i = 0; i < vals.size(); ++i)
+        ids.add(i);
+      Collections.sort(ids, (i, j) -> vals.get(i) - vals.get(j));
+      for (int i = 0; i < ids.size(); ++i)
+        for (; ids.get(i) != i; ++ans)
+          swap(ids, i, ids.get(i));
+    }
+    return ans;
+  }
+  private void swap(List<Integer> ids, int i, int j) {
+    final int temp = ids.get(i);
+    ids.set(i, ids.get(j));
+    ids.set(j, temp);
+  }
+}
