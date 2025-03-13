7bfddff089502a59532aec0ac2dcd69835eace4e
Sameem Ahamad <118834172+sameem74@users.noreply.github.com>
2025-03-13 10:53:46 +0530

    Add files via upload

---
+++ 0 - 1 Knapsack Problem - GFG/0 - 1 Knapsack Problem.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCases = Integer.parseInt(br.readLine());
+
+        while (testCases-- > 0) {
+            int capacity = Integer.parseInt(br.readLine());
+            String[] valInput = br.readLine().split(" ");
+            String[] wtInput = br.readLine().split(" ");
+
+            int[] val = new int[valInput.length];
+            int[] wt = new int[wtInput.length];
+
+            for (int i = 0; i < valInput.length; i++) {
+                val[i] = Integer.parseInt(valInput[i]);
+            }
+
+            for (int i = 0; i < wtInput.length; i++) {
+                wt[i] = Integer.parseInt(wtInput[i]);
+            }
+
+            System.out.println(Solution.knapsack(capacity, val, wt));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return max value that can be put in knapsack of capacity.
+    static int knapsack(int W, int val[], int wt[]) {
+        // code here
+        int N = wt.length;
+        int[][] dp = new int[N + 1][W + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= W; j++) {
+                if (wt[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], val[i - 1] + dp[i - 1][j - wt[i - 1]]);
+                }
+            }
+        }
+        return dp[N][W];
+    }
+}
